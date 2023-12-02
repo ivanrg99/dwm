c60a2d371891936414cbb1116c75c439d56fe213
Ivan <ivanrg99@tutanota.com>
2023-12-02 18:59:56 +0100

    only be able to fullscreen if at least 1 window is present

--- dwm.c
+++ dwm.c
@@ -1682,12 +1682,16 @@ Layout *last_layout;
 void
 fullscreen(const Arg *arg)
 {
+	if(selmon->sel == 0)
+		return;
+
 	if (selmon->showbar) {
 		for(last_layout = (Layout *)layouts; last_layout != selmon->lt[selmon->sellt]; last_layout++);
 		setlayout(&((Arg) { .v = &layouts[2] }));
 	} else {
 		setlayout(&((Arg) { .v = last_layout }));
 	}
+
 	if (selmon->sel->bw > 0) {
 		selmon->sel->bw = 0;
 	} else {
